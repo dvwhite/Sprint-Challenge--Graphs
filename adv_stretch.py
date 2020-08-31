09d76cd28dbc3f4c15291ef36811cd23834878b8
David White <dwhite0627@gmail.com>
2020-08-30 21:06:01 -0400

    Monte carlo methods to generate a shorter path using randomness, split main method out into a function

---
+++ adv_stretch.py
@@ -0,0 +1,77 @@
+from room import Room
+from player import Player
+from world import World
+
+import random
+from ast import literal_eval
+
+from utils import bfs_unexplored, get_quadrant, traverse_map, monte_carlo_paths
+import ipdb
+
+# Load world
+world = World()
+
+
+# You may uncomment the smaller graphs for development and testing purposes.
+# map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
+
+# Loads the map into a dictionary
+room_graph = literal_eval(open(map_file, "r").read())
+world.load_graph(room_graph)
+
+# Print an ASCII map
+world.print_rooms()
+
+player = Player(world.starting_room)
+
+# Fill this out with directions to walk
+# traversal_path = ['n', 'n']
+traversal_path = []
+
+# Monte Carlo method to check if the BFS/DFS solution can meet stretch
+# It can, but very rarely. The heuristic seems to involve finishing a
+# quadrant before moving on
+trials = []
+monte_carlo_paths([player.current_room, traversal_path], 100000, trials)
+min_path = min([(len(lst), idx) for idx, lst in enumerate(trials)])
+print("Min:", min_path)
+if min_path[0] < 960:
+    print(trials[min_path[1]])
+
+# Traverse the maze
+# traverse_map(player.current_room, traversal_path)
+# print(traversal_path)
+
+# TRAVERSAL TEST - DO NOT MODIFY
+visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
+
+for move in traversal_path:
+    player.travel(move)
+    visited_rooms.add(player.current_room)
+
+if len(visited_rooms) == len(room_graph):
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
+
+#######
+# UNCOMMENT TO WALK AROUND
+#######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import random
-#import ipdb
+# import ipdb
 
 
 def bfs_unexplored(starting_node, explored):
@@ -26,6 +26,112 @@ def bfs_unexplored(starting_node, explored):
                     # enqueue the exits
                     nodes = [
                         (path + [ex], node.get_room_in_direction(ex))] + nodes
+
+
+def bfs_unexplored_quad(starting_node, explored, world):
+    """Generate a BFS path to unexplored but preferring nodes in
+    the same quadrant"""
+
+    # Get the original quadrant
+    orig_quad = get_quadrant(starting_node, world)
+
+    # Exhaust that path
+    visited = []
+    nodes = [([], starting_node)]
+    while nodes:
+        # Get the next room in the rooms list
+        (path, node) = nodes.pop()
+
+        # Check that room's exits in the explored dict for unexplored rooms,
+        # which are indicated by a '?'
+        if node and node not in visited:
+            node_quad = get_quadrant(node, world)
+            exits = node.get_exits()
+            if '?' in explored[node.id].values():
+                for direction, dest in explored[node.id].items():
+                    if dest == '?' and node_quad == orig_quad:
+                        return path
+            else:
+                # Add node to the visited node arr
+                visited.append(node)
+
+                for ex in exits:
+                    # enqueue the exits
+                    nodes = [
+                        (path + [ex], node.get_room_in_direction(ex))] + nodes
+
+
+def traverse_map(starting_vertex, path):
+    # Traverse the map
+    # Compile the path throught the map in path
+
+    # Setup
+    explored = {}
+    reverse_dirs = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+    curr_room = starting_vertex
+    explored[curr_room.id] = {ex: '?' for ex in curr_room.get_exits()}
+
+    # Pick a random starting direction
+    rooms_to_visit = []
+    rooms_to_visit.append(random.choice(curr_room.get_exits()))
+
+    # Keep walking until you hit a dead end
+    # ipdb.set_trace()
+    while rooms_to_visit:
+        # Reset variables
+        unexplored = []
+
+        # Get the next room
+        prev_room = curr_room
+        next_room_dir = rooms_to_visit.pop()
+
+        # Explore the next room using graph
+        path.append(next_room_dir)
+        curr_room = curr_room.get_room_in_direction(next_room_dir)
+        curr_exits = curr_room.get_exits()
+
+        # Mark it as visited and create a new dict
+        explored[prev_room.id][next_room_dir] = curr_room.id
+        if curr_room.id not in explored:
+            explored[curr_room.id] = {ex: '?' for ex in curr_exits}
+
+        # Mark the room number of the opposite direction we came from as
+        # visited, as long as curr_rm connects back to it
+        reverse_dir = reverse_dirs[next_room_dir]
+        if reverse_dir in curr_exits:
+            explored[curr_room.id][reverse_dir] = prev_room.id
+
+        # Add all unexplored exits from the current room
+        unexplored = [ex for ex in explored[curr_room.id]
+                      if explored[curr_room.id][ex] == '?']
+
+        # If there's more rooms to explore, continue exploring them
+        if len(unexplored) > 0:
+            choice = random.choice(unexplored)
+            rooms_to_visit += [choice]
+
+        # Once the player hits a dead end, BFS to locate a path
+        # back to closest unexplored location
+        # Exhaust that path
+        # Repeat until all paths are explored
+        else:
+            path_to_unexplored = bfs_unexplored(curr_room, explored)
+            if not path_to_unexplored is None:
+                for direction in path_to_unexplored:
+                    path.append(direction)
+                    curr_room = curr_room.get_room_in_direction(direction)
+
+                # Add all unexplored exits from the current room
+                unexplored = [ex for ex in explored[curr_room.id]
+                              if explored[curr_room.id][ex] == '?']
+
+                # If there's more rooms to explore, continue exploring them
+                if len(unexplored) > 0:
+                    choice = random.choice(unexplored)
+                    rooms_to_visit += [choice]
+
+
 def get_quadrant(node, world):
     """Return the quadrant of the node in world's node grid"""
 
@@ -63,3 +169,12 @@ def get_quadrant(node, world):
     else:
         raise ValueError(
             f"Error! Node with id {node.id} was not found in a quadrant")
+
+
+def monte_carlo_paths(args, n_trials, results):
+    [starting_vertex, path] = [*args]
+    for idx in range(n_trials):
+        traverse_map(starting_vertex, path)
+        results.append(path.copy())
+        path.clear()
+    return results
